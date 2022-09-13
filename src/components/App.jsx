9d77ec3f5c2eaed2d47df4943044d2914b5b85bc
OspanovaVita <vitaospanova@ukr.net>
2022-09-13 22:48:42 +0300

    start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import user from 'data/user.json';
+
+// export default function App () {
+//   return (
+//     <Container>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}    
+//       />
+//     </Container>
+//   );
+// };
+// export const App = () => {
+//   return (
+//     <>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       <Statistics title="Upload stats" stats={statistic} />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />
+//     </>
+//   );
+// };
